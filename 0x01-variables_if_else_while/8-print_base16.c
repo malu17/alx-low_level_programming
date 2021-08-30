192d0738033e4d61ecdf3d00d2f453b0fd7f5611
Elias Fiseha <asbezah@gmail.com>
2021-08-30 12:56:58 +0300

    add 8-print_base16.c secript

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,16 +10,16 @@ int main(void)
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
-	{        
+	{
 		putchar(c);
 	}
 
 	for (c = 'a'; c <= 'f'; c++)
 	{
-	       putchar(c);
+		putchar(c);
 	}
 
 	putchar('\n');
 
-	return (0)
+	return (0);
 }
