f52725d7df20eb45bd3c93e1990b68a414915679
Eliyas Fisha <asezah@gmail.com>
2021-09-06 17:40:36 +0300

    add 4-print_rev.c secript

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_rev - check the code for School students.
+ * print_rev - check the code for alx School students.
  * @s: pointer int type
  * Return: Always 0.
  */
 void print_rev(char *s)
 {
-	int len;
-	for (len = 0; *s != '\0'; len++)
-		s++;
+int len;
+for (len = 0; *s != '\0'; len++)
+s++;
 
-	s--;
-	for (len = len; len != 0; len--)
-	{
-		_putchar(*s);
-		s--;
-	}
+s--;
+for (len = len; len != 0; len--)
+{
+_putchar(*s);
+s--;
+}
 
-	_putchar('\n');
+_putchar('\n');
 }
