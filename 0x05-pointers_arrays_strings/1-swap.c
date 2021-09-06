f9ec049f255c1d1a53f5b3965ae2937fc709d629
Eliyas Fisha <asezah@gmail.com>
2021-09-06 12:56:59 +0300

    add 1-swap.c secript

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * swap_int - check the code for Holberton School students.
+ * swap_int - check the code for alx School students.
  * @a: is a pointer int type
  * @b: is a pointer int type
  * Return: Always 0.
