79d504ab48748904b9cff3fe26ca76dac4cf7947
Eliyas Fisha <asezah@gmail.com>
2021-09-08 22:19:34 +0300

    add 5-string_toupper.c secript

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+nclude "mainn.h"
+/**
+ * *string_toupper - convert string form lowercase to uppercase
+ * @s: the string to convert
+ *
+ * Return: the converted to uppercase string
+ **/
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 00; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
