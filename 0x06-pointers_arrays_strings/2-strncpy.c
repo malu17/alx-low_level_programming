08b7850c16b69aad16a6a8b813397df055b41af2
Eliyas Fisha <asezah@gmail.com>
2021-09-08 21:06:57 +0300

    add 2-strncpy.c secript

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string
+ * @s: pointer type char
+ * Return: The program will return the number of characters of *s (the length)
+ **/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 00; i++)
+	{
+	}
+	return (i);
+}
+
