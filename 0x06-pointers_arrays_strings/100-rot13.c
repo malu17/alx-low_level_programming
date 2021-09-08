3fbbe1785eb6184ce1ba6d3f2f31abd02b7fdc87
Eliyas Fisha <asezah@gmail.com>
2021-09-08 22:45:50 +0300

    add 100-rot13.c secript

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,3 +6,21 @@
  * Return: string encoded
  **/
 char *rot13(char *s)
+{
+	int i, j;
+	char src[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char encode[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	for (i = 0; s[i] != 00; i++)
+	{
+		for (j = 0; src[j] != 00; j++)
+		{
+			if (s[i] == src[j])
+			{
+				s[i] = encode[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
