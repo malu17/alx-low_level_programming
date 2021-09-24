68087d37268851ac63328fcf97a7c8cd73ff39b6
Eliyas Fisha <asezah@gmail.com>
2021-09-24 09:15:18 +0300

    add updated 1-string_nconcat.c secript

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,58 +1,55 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * string_nconcat - Concatenate two strings
- * @s1: string 1
- * @s2: string 2
- * @n: unsigned int
- *Return: returned pointer shall point to a newly allocated space in memory,
- *which contains s1, followed by the first  *n bytes of s2, and null terminated
- *If the function fails, it should return NULL
- *If n is greater or equal to the length of s2 then use the entire string s2
- *if NULL is passed, treat it as an empty string
+ * _strlen - function that returns the length of a string.
+ * @s: s is a string.
+ * Return: length of string.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * string_nconcat - function that concatenates two strings.
+ * @s1: string s1.
+ * @s2: string s2.
+ * @n: bytes of s2.
+ * Return: Always 0.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int con, lon1, lon2;
-	char *constr;
+	char *str;
+	unsigned int l, t, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	lon1 = _strl(s1);
-	lon2 = _strl(s2);
+	t = _strlen(s1);
+	l = _strlen(s2);
+	if (n < l)
+		l = n;
 
-	if (n > lon2)
-		n = lon2;
+	str = malloc(t + l + 1);
 
-	constr = malloc(sizeof(char) * (lon1 + n + 1));
-	if (constr == NULL)
+	if (str == NULL)
+	{
 		return (NULL);
+	}
 
-	for (con = 0; con < lon1; con++)
-		constr[con] = s1[con];
+	for (i = 0; i < t; i++)
+		*(str + i) = *(s1 + i);
 
-	for (; con < lon1 + n; con++)
-		constr[con] = s2[con - lon1];
-
-	constr[lon1 + n] = '\0';
-	return (constr);
-}
-
-
-/**
- * _strl - Return length of string
- * @s: string
- * Return: length of the string
- */
-
-int _strl(char *s)
-{
-	int con;
-
-	for (con = 0; s[con] != '\0'; con++)
-		;
-	return (con);
+	for (i = 0; i < l; i++)
+		*(str + (i + t)) = *(s2 + i);
+	*(str + (i + t)) = '\0';
+	return (str);
 }
