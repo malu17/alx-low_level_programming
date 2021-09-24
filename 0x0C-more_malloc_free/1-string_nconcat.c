6f002abdd4755ab92a6b8e23667b8d4102fd8e6e
Eliyas Fisha <asezah@gmail.com>
2021-09-24 08:55:33 +0300

    updated 1-string_nconcat.c secript

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,48 @@
-#include "main.h"
 #include <stdlib.h>
+#include "holberton.h"
+/**
+ * _strlen - get length of string.
+ * @str: string.
+ * Return: length.
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
 
+	i = 0;
+	while (str[i] != '\0')
+	{
+		++i;
+	}
+	return (i);
+}
 /**
  * string_nconcat - concatenates two strings.
- * @s1: first string to copy
- * @s2: second string to copy
- * @n: number of bytes of s2 to copy
- *
- * Return: char pointer to newly allocated place in memory
+ * @s1: strging 1.
+ * @s2: string 2.
+ * @n: byets of s2 to include.
+ * Return: pointer to a string or NULL.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, k;
-	char *s;
+	char *str;
+	unsigned int len1, len2, i, j;
 
 	if (s1 == NULL)
-		i = 0;
-	else
-	{
-		for (i = 0; s1[i]; i++)
-			;
-	}
+		s1 = "";
 	if (s2 == NULL)
-		j = 0;
-	else
-	{
-		for (j = 0; s2[j]; j++)
-			;
-	}
-	if (j > n)
-		j = n;
-	s = malloc(sizeof(char) * (i + j + 1));
-	if (s == NULL)
+		s2 = "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (n >= len2)
+		n = len2;
+	str = malloc(len1 + n + 1 * sizeof(char));
+	if (str == NULL)
 		return (NULL);
-	for (k = 0; k < i; k++)
-		s[k] = s1[k];
-	for (k = 0; k < j; k++)
-		s[k + i] = s2[k];
-	s[i + j] = '\0';
-	return (s);
+	for (i = 0; i < len1; ++i)
+		str[i] = s1[i];
+	for (j = 0; j < n; ++j, ++i)
+		str[i] = s2[j];
+	str[i] = '\0';
+	return (str);
 }
