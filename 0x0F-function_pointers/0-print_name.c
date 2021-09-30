5a5540860aa0d343a2c5caac48a92dc2cb3b1c08
Eliyas Fisha <asezah@gmail.com>
2021-09-30 13:51:46 +0300

    updated 0-print_name.c secript

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 /**
  * print_name - prints a name
- * @name: string name
+ * @bob: string name
  * @f: function pointer to print name
  * Return: void
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *bob, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (bob && f)
+		f(bob);
 }
