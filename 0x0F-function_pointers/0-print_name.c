c1ca2662d2af10e9068e128471efa4ed688976e8
Eliyas Fisha <asezah@gmail.com>
2021-09-30 13:57:44 +0300

    updated 0-print_name.c secript

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,37 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
- * print_name - prints a name
- * @bob: string name
- * @f: function pointer to print name
- * Return: void
+ * print_elem - prints an integer
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
  */
-void print_name(char *bob, void (*f)(char *))
+void print_elem(int elem)
 {
-	if (bob && f)
-		f(bob);
+printf("%d\n", elem);
+}
+
+/**
+ * print_elem_hex - prints an integer, in hexadecimal
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
+void print_elem_hex(int elem)
+{
+printf("0x%x\n", elem);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int array[5] = {0, 98, 402, 1024, 4096};
+
+array_iterator(array, 5, &print_elem);
+array_iterator(array, 5, &print_elem_hex);
+return (0);
 }
