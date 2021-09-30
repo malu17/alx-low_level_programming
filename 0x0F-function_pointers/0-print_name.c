581a66d959c1ed0c80676fde213f50cb82b1d8ea
Eliyas Fisha <asezah@gmail.com>
2021-09-30 14:03:20 +0300

    updated 0-print_name.c secript

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,10 @@
-#include "function_pointers.h"
 /**
- * array_iterator - executes a function given as a paramete
- * @array: array of functions
- * @size: size of array
- * @action: function to execute
- * Description: executes a function given as a parameter of element of an array.
+ * print_name -  prints a name.
+ * @name: - name to print
+ * @f: - function
  */
-void array_iterator(int *array, size_t size, void (*action)(int))
+void print_name(char *name, void (*f)(char *))
 {
-unsigned int i = 0;
-
-for ( ; i < size ; i++)
-action(array[i]);
-
+	if (name && f)
+		f(name);
 }
