3c1d8304b1f99c64ace51ade35caafad7b80f0bd
Lesm <88344019+malu17@users.noreply.github.com>
2021-11-29 15:15:07 +0300

    updated 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/malu17/alxschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
-export LD_PRELOAD=/tmp/putshack.so
+wget -P /tmp/ https://raw.github.com/malu17/alxschool-low_level_programming/master/0x18-dynamic_libraries/libran.so
+export LD_PRELOAD=/tmp/libran.so
