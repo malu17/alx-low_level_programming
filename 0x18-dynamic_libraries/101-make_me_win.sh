1022c8fd4dc64e4db1b26b90e2d5d485b9c4750d
Lesm <88344019+malu17@users.noreply.github.com>
2021-11-29 15:17:22 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com//alxschool-low_level_programming/master/0x18-dynamic_libraries/libran.so
+wget -P /tmp/ https://raw.github.com/malu17/alxschool-low_level_programming/master/0x18-dynamic_libraries/libran.so
 export LD_PRELOAD=/tmp/libran.so
