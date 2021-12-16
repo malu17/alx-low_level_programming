d4af55c0c784f4cef4ec542b4a45b12815fcb4a9
Lesm <88344019+malu17@users.noreply.github.com>
2021-12-16 11:49:20 +0300

    Initializes all the array with NULL

--- 0x1A-hash_tables/6-main.c
+++ 0x1A-hash_tables/6-main.c
@@ -4,7 +4,7 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for alx School students.
  *
  * Return: Always EXIT_SUCCESS.
  */
@@ -17,10 +17,10 @@ int main(void)
 	ht = hash_table_create(1024);
 	hash_table_set(ht, "c", "fun");
 	hash_table_set(ht, "python", "awesome");
-	hash_table_set(ht, "Jennie", "and Jay love asm");
+	hash_table_set(ht, "gennie", "and Jay love asm");
 	hash_table_set(ht, "N", "queens");
-	hash_table_set(ht, "Asterix", "Obelix");
-	hash_table_set(ht, "Betty", "Holberton");
+	hash_table_set(ht, "Aster", "Obelix");
+	hash_table_set(ht, "Betty", "alx");
 	hash_table_set(ht, "98", "Battery Streetz");
 	key = strdup("Tim");
 	value = strdup("Britton");
@@ -30,10 +30,10 @@ int main(void)
 	free(key);
 	free(value);
 	hash_table_set(ht, "98", "Battery Street");
-	hash_table_set(ht, "hetairas", "Jennie");
-	hash_table_set(ht, "hetairas", "Jennie Z");
-	hash_table_set(ht, "mentioner", "Jennie");
-	hash_table_set(ht, "hetairas", "Jennie Z Chu");
+	hash_table_set(ht, "hetairas", "gennie");
+	hash_table_set(ht, "hetairas", "gennie Z");
+	hash_table_set(ht, "mentioner", "gennie");
+	hash_table_set(ht, "hetairas", "gennie Z Chu");
 	hash_table_print(ht);
 	hash_table_delete(ht);
 	return (EXIT_SUCCESS);
